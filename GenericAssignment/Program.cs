ab9468bb0d0dc1476af55870dbdebfc42ae9a2e7
omark96 <43138339+omark96@users.noreply.github.com>
2025-09-24 13:54:14 +0200

    Add project files.

---
+++ GenericAssignment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GenericAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
