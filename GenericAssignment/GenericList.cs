5704f54f76153da126cb832dedc96fde0e7965db
omark96 <43138339+omark96@users.noreply.github.com>
2025-09-24 14:08:38 +0200

    wip

---
+++ GenericAssignment/GenericList.cs
@@ -0,0 +1,26 @@
+﻿namespace GenericAssignment;
+internal class GenericList<T>
+{
+    T[] _items;
+    int count;
+    int defaultCapacity = 4;
+
+    public GenericList()
+    {
+        _items = new T[defaultCapacity];
+        count = 0;
+    }
+
+    public void Add(T item)
+    {
+        if (count == _items.Length)
+        {
+            T[] temp = new T[_items.Length * 2];
+            Array.Copy(_items, temp, _items.Length);
+            _items = temp;
+        }
+        _items[count] = item;
+
+        count++;
+    }
+}
--- GenericAssignment/Program.cs
+++ GenericAssignment/Program.cs
@@ -5,6 +5,13 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            List<int> lista = new();
+            GenericList<int> myList = new();
+            for (int i = 0; i < 20; i++)
+            {
+                myList.Add(i);
+                Console.WriteLine(i);
+            }
         }
     }
 }
